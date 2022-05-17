0d2a350e4d299bc76d28ca1a8ffe14ca8fa58379
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-05-17 11:06:00 +0530

    cycle-two

--- packages/apps/src/index.ts
+++ packages/apps/src/index.ts
@@ -9,17 +9,37 @@ import CompetitiveContestApp from "@felvin-community/competitive-contest-app";
 import Compressify from "@felvin-community/compressify";
 import Constants from "@felvin-community/constants";
 
+import ConvertToRoman from "@felvin-community/convert-to-roman";
+import CourierTracker from "@felvin-community/courier-tracker";
+import CronParser from "@felvin-community/cron-parser";
+import CropImage from "@felvin-community/crop-image";
+import Crypto from "@felvin-community/crypto";
+import csvToJson from "@felvin-community/csv-to-json";
+import currencyConvertor from "@felvin-community/currency-convertor";
+import DiceRoller from "@felvin-community/dice-roller";
+import Dictionary from "@felvin-community/dictionary";
+
 const allApps = [
   AddressPincodes,
   AsciiArt,
   Bmiconverter,
   bouncyBall,
-  capitals,
+  capitals,      //cycle-one
   CodeReference,
   ColorPicker,
   CompetitiveContestApp,
   Compressify,
   Constants,
+
+  ConvertToRoman,
+  CourierTracker,
+  CronParser,
+  CropImage,
+  Crypto,
+  csvToJson,
+  currencyConvertor,
+  DiceRoller,
+  Dictionary,
 ];
 
 export default allApps;
